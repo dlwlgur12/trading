c6563810cca4af9fd14acf0995d5e6f2a739ab52
Your Name <you@example.com>
2024-12-31 05:02:50 +0900

    JWT 인증 및 사용자 정보 API 수정

--- models/User.js
+++ models/User.js
@@ -1,27 +1,15 @@
 const mongoose = require('mongoose');
 
-// 사용자 스키마 정의
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   birth: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
   brokerage: { type: String, required: true },
-  account: { type: String, required: true },
-  stocks: [
-    {
-      symbol: { type: String },    // 주식 종목
-      name: { type: String },      // 주식 이름
-      logoUrl: { type: String },   // 주식 로고 URL
-      quantity: { type: Number, default: 0 },  // 보유 수량 (기본값 0)
-      assetValue: { type: Number, default: 0 }, // 자산 가치 (기본값 0)
-    }
-  ]
+  account: { type: String, required: true }
 });
 
-// User 모델 생성
-const User = mongoose.model('User', userSchema, 'users');
-
+const User = mongoose.model('User', userSchema);
 module.exports = User;
