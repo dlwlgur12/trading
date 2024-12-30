1f9778596d2ddd20a9c658aa6677164aaa275080
Your Name <you@example.com>
2024-12-31 05:09:25 +0900

    JWT 인증 및 사용자 정보 API 수정

--- models/User.js
+++ models/User.js
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   birth: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   brokerage: { type: String, required: true },
-  account: { type: String, required: true }
+  account: { type: String, required: true },
 });
+
+// 'id' 필드를 추가하는 대신 'username'이나 'email' 필드를 고유하게 설정
+// or 기본적으로 제공되는 '_id'를 그대로 사용할 수 있습니다.
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
